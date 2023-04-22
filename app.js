e97e314146cb510319452c0284754ab82ae28ffe
tejanhu <tejanahmedhu@gmail.com>
2023-04-22 03:50:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function findLargest(n, m){
+    alert(Math.max(n,m));
+}
+
+var msg = "";
+
+function isEvenOrOdd(n){
+    if(n%2==0){
+        msg = "Even!";
+    }
+    else{
+        msg = "Odd!";
+    }
+    alert(msg);
+}
